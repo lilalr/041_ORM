aaea9b6b58c176cc46d79dae24b6e71c7cd61db5
lilalr <gurennut@gmail.com>
2025-10-27 12:16:11 +0700

    membuat route get utk mengambil semua data komik

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const PORT = 3000;
 const db = require('./models');
+const { raw } = require('mysql2');
 
 app.use(express.json());
 app.use(express.urlencoded({ 
@@ -27,6 +28,15 @@ app.post("/komik", async(req, res)=>{
     const dara = req.body;
     try{
         const komik = await db.Komik.create(data);
+        res.send(komik);
+    } catch(err){
+        res.send(err);
+    }
+})
+
+app.get("/komik", async(req, res)=>{
+    try{
+        const komik = await db.Komik.findAll();
         res.send(komik);
     } catch(err){
         res.send(err);
